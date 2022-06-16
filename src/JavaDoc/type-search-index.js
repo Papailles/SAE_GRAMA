a0b7facd85bd3eaa2cfe6d416ff5b9b3b2416818
Papailles <77628699+Papailles@users.noreply.github.com>
2022-06-16 07:46:22 +0200

    JavaDoc

---
+++ src/JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"GRAMA","l":"GraphInterface"},{"p":"GRAMA","l":"GRAPHMAP"},{"p":"GRAMA","l":"runInterface"}];updateSearchResults();
